1474a6e3893b26bb01b432d1971393577449b00d
Blangas <blangas2000@yahoo.com>
2025-06-25 11:26:08 +0200

    Interactable script checks players raycast

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public RaycastFromPlayer raycastFromPlayer;
+    private RaycastFromPlayer raycastFromPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        raycastFromPlayer = GameObject.FindWithTag("Player").GetComponent<RaycastFromPlayer>();
+        raycastFromPlayer = GameObject.FindWithTag("Player").GetComponent<RaycastFromPlayer>(); //pick that raycast script from the player
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (raycastFromPlayer.targetHit == transform) // If the raycast hits this object
+        {
+
+        }
     }
 }
